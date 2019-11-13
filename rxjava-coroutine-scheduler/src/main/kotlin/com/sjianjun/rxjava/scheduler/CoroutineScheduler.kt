dbf6c79066969867e1bff96be739b2bb01a0b6c5
sjianjun <978622005@qqcom>
2019-11-13 21:20:16 +0800

    ....

--- rxjava-coroutine-scheduler/src/main/kotlin/com/sjianjun/rxjava/scheduler/CoroutineScheduler.kt
+++ rxjava-coroutine-scheduler/src/main/kotlin/com/sjianjun/rxjava/scheduler/CoroutineScheduler.kt
@@ -4,12 +4,13 @@ import io.reactivex.Scheduler
 import io.reactivex.disposables.Disposable
 import io.reactivex.internal.disposables.EmptyDisposable
 import io.reactivex.internal.disposables.SequentialDisposable
+import io.reactivex.internal.queue.MpscLinkedQueue
 import io.reactivex.plugins.RxJavaPlugins
 import kotlinx.coroutines.*
-import java.util.*
 import java.util.concurrent.CountDownLatch
 import java.util.concurrent.TimeUnit
 import java.util.concurrent.atomic.AtomicBoolean
+import java.util.concurrent.atomic.AtomicInteger
 import java.util.concurrent.atomic.AtomicReference
 import kotlin.coroutines.CoroutineContext
 
@@ -23,7 +24,8 @@ class CoroutineScheduler(private val context: CoroutineContext = Dispatchers.IO)
         @Volatile
         private var disposed: Boolean = false
         private val coroutine = CoroutineScope(context)
-        private val delayCoroutine = CoroutineScope(Dispatchers.Unconfined)
+        private val scheduleQueue: MpscLinkedQueue<ScheduledRunnable> = MpscLinkedQueue()
+        private val wip = AtomicInteger()
 
         private val seqCountDownLatch = AtomicReference<CountDownLatch?>()
         override fun schedule(runnable: Runnable): Disposable {
@@ -65,7 +67,8 @@ class CoroutineScheduler(private val context: CoroutineContext = Dispatchers.IO)
             }
             val sequentialDisposable = SequentialDisposable()
             val decoratedRun = ScheduledRunnable(RxJavaPlugins.onSchedule(runnable))
-            val job = delayCoroutine.launch {
+            scheduleQueue.offer(decoratedRun)
+            coroutine.launch {
                 try {
                     if (l > 0) {
                         delay(timeUnit.toMillis(l))
@@ -78,7 +81,7 @@ class CoroutineScheduler(private val context: CoroutineContext = Dispatchers.IO)
                     RxJavaPlugins.onError(e)
                 }
             }
-            sequentialDisposable.replace(CoroutineDispose(job, decoratedRun))
+            sequentialDisposable.replace(decoratedRun)
             return sequentialDisposable
         }
 
@@ -86,7 +89,6 @@ class CoroutineScheduler(private val context: CoroutineContext = Dispatchers.IO)
             if (!disposed) {
                 disposed = true
                 coroutine.cancel()
-                delayCoroutine.cancel()
             }
         }
 
@@ -130,29 +132,6 @@ class CoroutineScheduler(private val context: CoroutineContext = Dispatchers.IO)
             } finally {
                 lazySet(true)
             }
-        }
-    }
-
-    private class SeqQueue : LinkedList<ScheduledRunnable>() {
-        override fun add(element: ScheduledRunnable): Boolean {
-            if (size > 0) {
-                synchronized(this) {
-                    var index = size - 1
-                    while (index >= 0) {
-                        if (element.time >= get(index).time) {
-                            index++
-                            break
-                        } else if (index == 0) {
-                            break
-                        }
-
-                        index--
-                    }
-                    super.add(index, element)
-                    return true
-                }
-            }
-            return super.add(element)
         }
     }
 
